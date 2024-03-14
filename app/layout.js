f577c483277b92382e32aa85e8d3802af47ff908
ezecodes <>
2024-03-14 04:55:44 +0100

    use poppins for main font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
-import { Raleway } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./normalize.css";
 import "./globals.css";
 
-const raleway = Raleway({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"]});
 
 export const metadata = {
   title: "Acadoze",
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={raleway.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
