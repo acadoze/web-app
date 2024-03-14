30b2adc786b21f9893e84fcbb634a5be85701775
ezecodes <>
2024-03-14 16:22:01 +0100

    modify footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] });
 
 export default function Footer() {
   return (
-    <footer className="bg-blue site_footer py-[2.5rem]">
+    <footer className={`bg-blue site_footer py-[2.5rem] ${roboto.className}`}>
       <div className="flex justify-evenly">
         <div>
           <h6>Acadoze</h6>
-          <li>Home</li>
-          <li>About</li>
-          <li>Career</li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/">About</Link></li>
+          <li><Link href="/">Career</Link></li>
         </div>
         <div>
           <h6>Legal</h6>
@@ -19,16 +21,15 @@ export default function Footer() {
         </div>
         <div>
           <h6>Contact</h6>
-          <li>Email</li>
-          <li>Blog</li>
-          <li>Chat</li>
+          <li><a>Email</a></li>
+          <li><a>Blog</a></li>
+          <li><a>Chat</a></li>
         </div>
         <div>
           <h6>Join our newsletter</h6>
-          <li className=" mb-[15px]">Enter your email here *</li>
-          <li>
-            <input className="px-3 py-4 mr-2 border-1 border-[#312213] w-[250px] rounded-sm" />
-            <button className="bg-[#bada55] py-[10px] px-[20px] text-white rounded-[100px]">Send</button>
+          <li className={`${roboto.className}`}>
+            <input className="px-2 py-2 mr-2 border-1 text-[white] bg-[#659bd5] border-[#312213] w-[250px] rounded-[60px]" />
+            <button className={`${roboto.className} bg-[#00c2ff] relative text-[white] right-[82px] px-3 py-2 text-[.95rem] text-white rounded-[60px]`}>Submit</button>
           </li>
           
         </div>
