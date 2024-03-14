38eecb2d7029bf661d7dd2c517cc90c518a3dd07
ezecodes <>
2024-03-14 04:57:15 +0100

    add api base variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_BASE: process.env.NEXT_PUBLIC_BASE_API
+  }
+};
 
 export default nextConfig;
