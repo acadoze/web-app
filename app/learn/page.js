c20b6a177d9faea87e80c0d9fc34a8d69b06822b
ezecodes <>
2024-03-16 19:47:15 +0100

    add character and background model model

--- app/learn/page.js
+++ app/learn/page.js
@@ -1,20 +1,17 @@
 "use client"
 import StudentHeader from "@/components/StudentHeader"
-import Footer from "@/components/Footer"
-import {useState} from 'react'
-import Link from "next/link";
-import { FaRegPlayCircle } from "react-icons/fa";
-import { FaFacebookF, FaLinkedinIn  } from "react-icons/fa6";
-import { BsInstagram } from "react-icons/bs";
-import { IoLogoTwitter } from "react-icons/io";
+import {useState, useEffect} from 'react'
+import Experience from "@/components/Player/Experience"
+import {useConvaiClient} from "../../hooks/useConvaiClient"
 
-import { Orbitron, Poppins, Outfit } from "next/font/google";
-const orbitron = Orbitron({ subsets: ["latin"] });
-const poppins = Poppins({ subsets: ["latin"], weight: ["200", "400", "500", "600", "700", "800", "900"]});
+import { Outfit } from "next/font/google";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export default function Home() {
   const [studentName, setName] = useState("Alex");
+  useEffect(() => {
+    
+  }, [])
 
   return (
     <main className="">
@@ -25,13 +22,10 @@ export default function Home() {
           <h2 className={`${outfit.className} absolute top-[80%] text-[1.5rem] text-[white] font-[600]`}> HISTORY </h2>
         </div>
 
-        <div className="flex flex-col  text-[white] x_spacing ">
-          <div className="flex justify-start gap-x-[5px] topics">
-            
-          </div>
+        <div className="w-[600px] h-[400px] mx-[auto] bg-[white] rounded-2xl">
+          <Experience />
         </div>
       </section>
     </main>
   );
 }
-
