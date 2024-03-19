f7c961e20936875b3dc88660cf8bdecde272753b
ezecodes <>
2024-03-19 11:57:59 +0100

    add animations

--- components/LearningMode/Tutor/Teacher.jsx
+++ components/LearningMode/Tutor/Teacher.jsx
@@ -1,72 +1,113 @@
-import {useGLTF} from "@react-three/drei"
+import {useGLTF, useAnimations} from "@react-three/drei"
 import {useStore} from "@/hooks/useStore"
 import { useFrame } from "@react-three/fiber";
-import { MathUtils } from "three";
-import { useEffect, useState } from "react";
+import { MathUtils,MeshStandardMaterial  } from "three";
+import { useEffect, useState, useRef } from "react";
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const ANIMATION_FADE_TIME = 0.5;
 
 export default function Teacher({teacher, ...props}) {
-	const {scene} = useGLTF(`/models/Teacher_Becky.glb`)
+	const {scene} = useGLTF(`/models/Teacher_Nanami.glb`)
   const chatPlayerObject = useStore(state => state.chatPlayerObject);
 	const [blink, setBlink] = useState(false)
+	  const group = useRef();
+
+	const { animations } = useGLTF(`/animations/animations_Nanami.glb`);
+	 const { actions, mixer } = useAnimations(animations, group);
+  	const [animation, setAnimation] = useState("Idle");
+
+  	 useEffect(() => {
+	    scene.traverse((child) => {
+	      if (child.material) {
+	        child.material = new MeshStandardMaterial({
+	          map: child.material.map,
+	        });
+	      }
+	    });
+	  }, [scene])
+	useEffect(() => {
+    let blinkTimeout;
+    const nextBlink = () => {
+      blinkTimeout = setTimeout(() => {
+        setBlink(true);
+        setTimeout(() => {
+          setBlink(false);
+          nextBlink();
+        }, 100);
+      }, randInt(1000, 5000));
+    };
+    nextBlink();
+    return () => clearTimeout(blinkTimeout);
+  }, []);
 
 	useEffect(() => {
-		let blinkTimeout
-		const nextBlink = () => {
-			blinkTimeout = setTimeout(() => {
-				setBlink(true)
-				setTimeout(() => {
-					setBlink(false)
-					nextBlink()
-				})
-			})
-		};
-		nextBlink()
-		return () => clearTimeout(blinkTimeout)
-	}, [])
+    actions[animation]
+      ?.reset()
+      .fadeIn(mixer.time > 0 ? ANIMATION_FADE_TIME : 0)
+      .play();
+    return () => {
+      actions[animation]?.fadeOut(ANIMATION_FADE_TIME);
+    };
+  }, [animation, actions]);
 	
 	const lerpMorphTarget = (target, value, speed = 0.1) => {
-		scene.traverse(child => {
-			if (child.isSkinnedMesh && child.morphTargetDictionary) {
-				const index = child.morphTargetDictionary[target]
-				if (
-					index === undefined ||
-					child.morphTargetInfluences[index] === undefined
-				) {
-					return
-				}
-				child.morphTargetInfluences[index] = MathUtils.lerp(
-					child.morphTargetInfluences[index],
-					value,
-					speed
-				)
-			}
-		})
-	}
-	useFrame(() => {
+    scene.traverse((child) => {
+      if (child.isSkinnedMesh && child.morphTargetDictionary) {
+        const index = child.morphTargetDictionary[target];
+        if (
+          index === undefined ||
+          child.morphTargetInfluences[index] === undefined
+        ) {
+          return;
+        }
+        child.morphTargetInfluences[index] = MathUtils.lerp(
+          child.morphTargetInfluences[index],
+          value,
+          speed
+        );
+      }
+    });
+  };
+	useFrame(({ camera }) => {
+    // Smile
+    lerpMorphTarget("mouthSmile", 0.2, 0.5);
+    // Blinking
+    lerpMorphTarget("eye_close", blink ? 1 : 0, 0.5);
 
-		lerpMorphTarget("eye_close", blink ? 1 : 0, 0.5)
-		
-		for (let i=0; i<= 21; i++) {
-			lerpMorphTarget(i, 0, 0.1)
-		}
-		if (
-			chatPlayerObject &&
-			chatPlayerObject.visemes &&
-			chatPlayerObject.audioPlayer
-		) {
-			for (let i = chatPlayerObject.visemes.length -1; i>=0; i--) {
-				const viseme = chatPlayerObject.visemes[i]
-				if (chatPlayerObject.audioPlayer.currentTime * 1000 >= viseme[0]) {
-					lerpMorphTarget(viseme[0], 1, 0.2)
-					break
-				}
-			}
-		}
-	})
+    // Talking
+    for (let i = 0; i <= 21; i++) {
+      lerpMorphTarget(i, 0, 0.1); // reset morph targets
+    }
+
+    if (
+      chatPlayerObject &&
+      chatPlayerObject.visemes &&
+      chatPlayerObject.audioPlayer
+    ) {
+      for (let i = chatPlayerObject.visemes.length - 1; i >= 0; i--) {
+        const viseme = chatPlayerObject.visemes[i];
+        if (chatPlayerObject.audioPlayer.currentTime * 1000 >= viseme[0]) {
+          lerpMorphTarget(viseme[1], 1, 0.2);
+          break;
+        }
+      }
+      if (
+        actions[animation].time >
+        actions[animation].getClip().duration - ANIMATION_FADE_TIME
+      ) {
+        setAnimation((animation) =>
+          animation === "Talking" ? "Talking2" : "Talking"
+        ); // Could load more type of animations and randomization here
+      }
+    }
+  });
 	
-	return <group {...props}> 
+	return <group {...props} dispose={null} ref={group}> 
 		<primitive object={scene} />
 	</group>
 }
 
-useGLTF.preload(`/models/Teacher_Becky.glb`)
+useGLTF.preload(`/models/Teacher_Nanami.glb`)
+  useGLTF.preload(`/animations/animations_Nanami.glb`);
